91405fa486acd6658548f2e0dc1fefc4f7330b91
Brandon Blaylock <brandon@null.pub>
2019-09-12 12:51:27 -0700

    fix: set view height of pending component

--- src/components/Async/Pending.tsx
+++ src/components/Async/Pending.tsx
@@ -14,7 +14,7 @@ export const Pending: FunctionalComponent<PendingProps> = ({
   title = 'Loading',
 }) => {
   return (
-    <section className="ct-b1 pwa-4 fld-row ai-ctr jc-ctr">
+    <section className="ct-b1 pwa-4 fld-row ai-ctr jc-ctr vh-4">
       <h3>{title}</h3>
     </section>
   );
