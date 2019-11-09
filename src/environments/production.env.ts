041dcc9c17cb8863f56db2c6220e0630dd67194c
Brandon Blaylock <brandon@null.pub>
2019-11-09 13:32:29 -0800

    fix: fix versionUrl to point to gitlab

--- src/environments/production.env.ts
+++ src/environments/production.env.ts
@@ -5,6 +5,6 @@ export const environment: Environment = {
   useMocks: false,
   showLogs: false,
   refreshInterval: 5 * 1000,
-  versionUrl: 'https://github.com/baetheus/blaylock-dev/releases/tag/v',
+  versionUrl: 'https://gitlab.com/baetheus/blaylock-dev/-/tags/v',
   version: process.env.CI_COMMIT_TAG || 'Unknown',
 };
